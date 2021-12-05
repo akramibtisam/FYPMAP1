467f2f0a0bfebeeee6b7c5fd180fa6bb0b3050e7
akramibtisam <ibtisamakram5@gmail.com>
2021-12-05 19:00:29 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import Header from "./Components/Header";
+import "./App.css";
+import Dbody from "./Components/Dbody";
+import SignIn from "./Components/SignIn";
+import SignInform from "./Components/form";
+import AdminLogin from "./Components/AdminSignin";
+import "./firebase/firebase";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Map from "./Components/Map";
+import Area_guide from "./Components/Area_guide";
+import MapData from "./Components/MapData";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route exact path="/SignInform">
+            <SignInform />
+          </Route>
+          <Route exact path="/Dashboard">
+            <Header />
+            <Dbody />
+          </Route>
+          <Route exact path="/Home">
+            <Home />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/adminlogin">
+            <AdminLogin />
+          </Route>
+          <Route exact path="/Map">
+            <Map />
+          </Route>
+          <Route path="/areaguides">
+            <Area_guide />
+          </Route>
+          <Route path="/mapdata">
+            <MapData/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/MapData.js
@@ -0,0 +1,9 @@
+function MapData() {
+    return (
+        <div>
+            <h1>Welcome to this page</h1>
+        </div>
+    )
+}
+
+export default MapData
